e738e30c7ed814541f934ec35b73e576fef356cc
its-dv <mishavelev1807@gmail.com>
2026-04-01 02:16:15 +0200

    Added script.js as a separate file

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js - index.html
+const textarea = document.getElementById("notepad");
+
+// Save text in localStorage
+function saveText() {
+  localStorage.setItem("notepadContent", textarea.value);
+  alert("Saved!");
+}
+
+// Load text from localStorage
+function loadText() {
+  const saved = localStorage.getItem("notepadContent");
+  alert("Loaded!");
+  if (saved !== null) {
+    textarea.value = saved;
+  } else {
+    alert("Nothing saved yet.");
+  }
+}
+
+// Auto-load when the page is opened
+window.onload = () => {
+  const saved = localStorage.getItem("notepadContent");
+  if (saved) {
+    textarea.value = saved;
+  }
+};
